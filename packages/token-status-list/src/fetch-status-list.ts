f972e08cf8e6779c2aa50c005ce474f46be19b02
Berend Sliedrecht <berend@animo.id>
2026-05-22 13:03:36 +0200

    chore: error on 399 and above status codes
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- packages/token-status-list/src/fetch-status-list.ts
+++ packages/token-status-list/src/fetch-status-list.ts
@@ -1,9 +1,10 @@
+import { SLException } from './status-list-exception'
 import { MediaTypes } from './types'
 
 export const fetchStatusList = async ({
   uri,
   customFetcher = fetch,
-  requiredFormat,
+  preferredFormat,
 }: {
   uri: string
   /**
@@ -11,46 +12,32 @@ export const fetchStatusList = async ({
    * If none is supplied either can be returned
    *
    */
-  requiredFormat?: 'cwt' | 'jwt'
+  preferredFormat?: 'cwt' | 'jwt'
   customFetcher?: typeof fetch
 }): Promise<string | Uint8Array> => {
-  if (requiredFormat === 'cwt') {
-    return fetchCwtStatusList(uri, customFetcher)
-  }
-  if (requiredFormat === 'jwt') {
-    return fetchJwtStatusList(uri, customFetcher)
-  }
   try {
-    return fetchJwtStatusList(uri, customFetcher)
-  } catch {
-    try {
-      return fetchCwtStatusList(uri, customFetcher)
-    } catch {
-      throw new Error('Could not fetch either a JWT or CWT as status list')
+    const response = await customFetcher(uri, {
+      headers: {
+        Accept:
+          preferredFormat === 'jwt'
+            ? `${MediaTypes.StatusListJwt}, ${MediaTypes.StatusListCwt}`
+            : `${MediaTypes.StatusListCwt}, ${MediaTypes.StatusListJwt}`,
+      },
+    })
+
+    if (response.status > 399 || response.status <= 199) {
+      throw new Error(`Could not fetch status list, response status '${response.status}'`)
     }
+
+    const contentType = response.headers.get('Content-type')
+    if (contentType === MediaTypes.StatusListJwt) {
+      return await response.text()
+    } else if (contentType === MediaTypes.StatusListCwt) {
+      return await (await response.blob()).bytes()
+    }
+
+    throw new SLException('Content type was either not provided in the response or invalid.')
+  } catch (e) {
+    throw new Error(`Could not fetch either a JWT or CWT as status list. ${(e as Error).message}`)
   }
-}
-
-const fetchJwtStatusList = async (uri: string, customFetcher: typeof fetch) => {
-  const response = await customFetcher(uri, {
-    headers: {
-      Accept: MediaTypes.StatusListJwt,
-    },
-  })
-
-  const token = await response.text()
-
-  return token
-}
-
-const fetchCwtStatusList = async (uri: string, customFetcher: typeof fetch) => {
-  const response = await customFetcher(uri, {
-    headers: {
-      Accept: MediaTypes.StatusListCwt,
-    },
-  })
-
-  const token = await (await response.blob()).bytes()
-
-  return token
 }
